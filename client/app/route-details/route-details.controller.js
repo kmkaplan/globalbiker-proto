d9df8258134cfd3431569173bd2eae773d3bc910
toub <toub@ldlc>
2014-08-12 16:21:59 +0200

    Enable leaflet map edition using leaflet.draw

--- client/app/route-details/route-details.controller.js
+++ client/app/route-details/route-details.controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('bikeTouringMapApp')
-    .controller('RouteDetailsCtrl', function ($scope) {
+    .controller('RouteDetailsCtrl', function ($scope, leafletData) {
         $scope.message = 'Hello';
 
-       
+
         angular.extend($scope, {
             defaults: {
                 // tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
@@ -17,6 +17,56 @@ angular.module('bikeTouringMapApp')
                 lng: 2.3648,
                 zoom: 8
             }
+        });
+
+        leafletData.getMap('main-map').then(function (map) {
+
+            // Initialise the FeatureGroup to store editable layers
+            var drawnItems = new L.FeatureGroup();
+            map.addLayer(drawnItems);
+
+            // Initialise the draw control and pass it the FeatureGroup of editable layers
+            var drawControl = new L.Control.Draw({
+                edit: {
+                    featureGroup: drawnItems,
+                    edit: {
+                        selectedPathoptions: {
+                            maintainColor: true,
+                            opacity: 0.3
+                        }
+                    },
+                    remove: false
+                },
+                draw: {
+                    polyline: {
+                        shapeOptions: {
+                            color: 'purple'
+                        }
+                    },
+                    marker: {
+                        shapeOptions: {
+                            color: 'purple'
+                        }
+                    },
+                    circle: false,
+                    rectangle: false,
+                    polygon: false
+                }
+            });
+            map.addControl(drawControl);
+
+            map.on('draw:created', function (e) {
+                var type = e.layerType,
+                    layer = e.layer;
+
+                if (type === 'marker') {
+                    // Do marker specific actions
+                }
+
+                // Do whatever else you need to. (save to db, add to map etc)
+                drawnItems.addLayer(layer);
+            });
+
         });
 
     });
