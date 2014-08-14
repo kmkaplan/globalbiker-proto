7a3cd0cd93af3661a026f629cf4a663700bd7204
MatthieuG <gaudetmatthieu@gmail.com>
2014-08-14 18:04:45 +0200

    Landing page presentation.

--- client/components/smooth-page-scroll/smooth-page-scroll.js
+++ client/components/smooth-page-scroll/smooth-page-scroll.js
@@ -1,8 +1,10 @@
-   function smoothScroll(targetLink) {
-        if (targetLink.length) {
-            $('html,body').animate({
-                scrollTop: $("#"+ targetLink).offset().top
-            }, 1000);
-            return false;
-        }
+   function smoothScroll(targetLink, timeout) {
+       setTimeout(function () {
+            if (targetLink.length) {
+                $('html,body').animate({
+                    scrollTop: $("#"+ targetLink).offset().top
+                }, 1000);
+                return false;
+            }
+       }, timeout === null ? 1 : timeout);
     };
