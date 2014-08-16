aac94d83032f8e429c45c17afd385c07872c7be0
MatthieuG <gaudetmatthieu@gmail.com>
2014-08-16 10:15:59 +0200

    Daft mailing service

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -3,11 +3,8 @@
 angular.module('bikeTouringMapApp')
   .controller('HomeCtrl', function ($scope) {
         $scope.addMail = function() {
-            alert("OUI");
             var mailingRepository;
-            mailingRepository = new MailingRepository({
-
-                })
+            mailingRepository = new MailingRepository();
             mailingRepository.addMail();
         }
   });
