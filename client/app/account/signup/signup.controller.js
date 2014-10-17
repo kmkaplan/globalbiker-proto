977857ee6f33ae533ce7455b33396b35cbf6e3f7
Nicolas Toublanc <n.toublanc@gmail.com>
2014-10-17 15:46:09 +0200

    update signin page

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -1,37 +1,45 @@
 'use strict';
 
 angular.module('bikeTouringMapApp')
-  .controller('SignupCtrl', function ($scope, Auth, $location, $window) {
-    $scope.user = {};
-    $scope.errors = {};
+    .controller('SignupCtrl', function ($scope, Auth, $location, $window, MailingRepository) {
+        $scope.user = {};
+        $scope.errors = {};
+        $scope.addMail = function ($email, $) {
+            var mailingRepository;
+            mailingRepository = new MailingRepository({
+                email: $email
+            });
+            mailingRepository.$save(function () {
+                $scope.mailSuccess = true;
+            });
+        }
+        $scope.register = function (form) {
+            $scope.submitted = true;
 
-    $scope.register = function(form) {
-      $scope.submitted = true;
+            if (form.$valid) {
+                Auth.createUser({
+                    name: $scope.user.name,
+                    email: $scope.user.email,
+                    password: $scope.user.password
+                })
+                    .then(function () {
+                        // Account created, redirect to home
+                        $location.path('/');
+                    })
+                    .catch(function (err) {
+                        err = err.data;
+                        $scope.errors = {};
 
-      if(form.$valid) {
-        Auth.createUser({
-          name: $scope.user.name,
-          email: $scope.user.email,
-          password: $scope.user.password
-        })
-        .then( function() {
-          // Account created, redirect to home
-          $location.path('/');
-        })
-        .catch( function(err) {
-          err = err.data;
-          $scope.errors = {};
+                        // Update validity of form fields that match the mongoose errors
+                        angular.forEach(err.errors, function (error, field) {
+                            form[field].$setValidity('mongoose', false);
+                            $scope.errors[field] = error.message;
+                        });
+                    });
+            }
+        };
 
-          // Update validity of form fields that match the mongoose errors
-          angular.forEach(err.errors, function(error, field) {
-            form[field].$setValidity('mongoose', false);
-            $scope.errors[field] = error.message;
-          });
-        });
-      }
-    };
-
-    $scope.loginOauth = function(provider) {
-      $window.location.href = '/auth/' + provider;
-    };
-  });
+        $scope.loginOauth = function (provider) {
+            $window.location.href = '/auth/' + provider;
+        };
+    });
--- server/api/mailing/index.js
+++ server/api/mailing/index.js
@@ -6,6 +6,6 @@ var controller = require('./mailing.controller');
 var auth = require('../../auth/auth.service');
 var router = express.Router();
 
-router.post('/', auth.hasRole('admin'), controller.create);
+router.post('/', controller.create);
 
 module.exports = router;
