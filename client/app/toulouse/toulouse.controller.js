09b98b095164816450f908db11dcee1939d98631
Nicolas Toublanc <n.toublanc@gmail.com>
2014-12-02 16:18:05 +0100

    Refactor ToulouseCtrl

--- client/app/toulouse/toulouse.controller.js
+++ client/app/toulouse/toulouse.controller.js
@@ -1,9 +1,17 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular.module('globalbikerWebApp')
-    .controller('ToulouseCtrl', function ($scope, $q, $state, Auth, TourRepository, StepRepository, InterestRepository, BikelaneRepository, bikeTourMapService, LicenseRepository, tourLoaderService) {
+    angular.module('globalbikerWebApp').controller('ToulouseCtrl', ToulouseCtrl);
+
+    function ToulouseCtrl($scope, $q, $state, Auth, bikeTourMapService, tourLoaderService) {
+
+        /* jshint validthis: true */
+        var vm = this;
 
         $scope.isAdmin = Auth.isAdmin;
+
+        $scope.openStep = openStep;
+        $scope.openTour = openTour;
 
         $scope.isAllowedToEdit = function (tour) {
             if (tour && Auth.isLoggedIn() && (Auth.isAdmin() || tour.userId === Auth.getCurrentUser()._id)) {
@@ -12,85 +20,43 @@ angular.module('globalbikerWebApp')
             return false;
         }
 
-        $scope.mapConfig = {
-            class: 'toulouse-map',
-            initialCenter: {
-                lat: 43.6,
-                lng: 1.45,
-                zoom: 11
-            },
-            callbacks: {
-                'map:created': function (eMap) {
+        $scope.mapConfig;
 
-                    $scope.openStep = function (step) {
-                        $state.go('step-details', {
-                            id: step._id
-                        }, {
-                            inherit: false
-                        });
-                    }
-                    $scope.openTour = function (tour) {
-                        if (tour.steps && tour.steps.length === 1) {
-                            $scope.openStep(tour.steps[0]);
-                        } else {
-                            $state.go('tour-details', {
-                                id: tour._id
-                            }, {
-                                inherit: false
-                            });
-                        }
-                    }
+        init();
 
-                    $scope.$watch('tours', function (tours, old) {
-                        if (tours) {
+        function init() {
 
-                            var toursToDisplayInMap = tours.reduce(function (toursToDisplayInMap, tour) {
-                                if (tour.priority === 1) {
-                                    toursToDisplayInMap.push(tour);
-                                }
-                                return toursToDisplayInMap;
-                            }, []);
+            $scope.isAdmin = Auth.isAdmin;
 
-                            var traceFeatures = bikeTourMapService.buildToursStepTracesFeatures(toursToDisplayInMap, {
-                                style: {
-                                    width: 3,
-                                    weight: 6,
-                                    opacity: 0.8
-                                },
-                                overStyle: {
-                                    color: '#34a0b4',
-                                    opacity: 1
-                                },
-                                callbacks: {
-                                    'click': function (step, tour) {
-                                        $scope.openTour(tour);
-                                    }
-                                },
-                                label: function (step, tour) {
-                                    return tour.title;
-                                }
-                            });
+            loadTourdDetails().then(function (tours) {
+                $scope.tours = tours;
+            });
 
-                            eMap.addItemsToGroup(traceFeatures, {
-                                name: 'Tracés des itinéraires',
-                                control: true
-                            });
+            $scope.mapConfig = initMapConfig();
 
-                        }
-                    });
+        }
 
-                },
-                'step:clicked': function (step, eMap, item, itemLayer, e) {
-                    $state.go('tour-details', {
-                        id: step.tourId
-                    }, {
-                        inherit: false
-                    });
-                }
-            },
-        };
-    
-        $scope.loadTourdDetails = function () {
+        function openStep(step) {
+            $state.go('step-details', {
+                id: step._id
+            }, {
+                inherit: false
+            });
+        }
+
+        function openTour(tour) {
+            if (tour.steps && tour.steps.length === 1) {
+                $scope.openStep(tour.steps[0]);
+            } else {
+                $state.go('tour-details', {
+                    id: tour._id
+                }, {
+                    inherit: false
+                });
+            }
+        }
+
+        function loadTourdDetails() {
             var deffered = $q.defer();
 
             tourLoaderService.loadToursWithDetails({
@@ -113,15 +79,74 @@ angular.module('globalbikerWebApp')
             return deffered.promise;
         };
 
+        function initMapConfig() {
+            return {
+                class: 'toulouse-map',
+                initialCenter: {
+                    lat: 43.6,
+                    lng: 1.45,
+                    zoom: 11
+                },
+                callbacks: {
+                    'map:created': function (eMap) {
 
-        $scope.init = function () {
-            $scope.isAdmin = Auth.isAdmin;
+                        $scope.$watch('tours', function (tours, old) {
 
-            $scope.loadTourdDetails().then(function (tours) {
-                $scope.tours = tours;
-            });
+                            var features = buildFeaturesFromTours(tours);
+
+                            if (features) {
+                                eMap.addItemsToGroup(features, {
+                                    name: 'Tracés des itinéraires',
+                                    control: true
+                                });
+                            }
+                        });
+
+                    },
+                    'step:clicked': function (step, eMap, item, itemLayer, e) {
+                        openStep(step);
+                    }
+                },
+            };
         }
 
-        $scope.init();
+        function buildFeaturesFromTours(tours) {
+            var features = null;
 
-    });
+            if (tours) {
+
+                var toursToDisplayInMap = tours.reduce(function (toursToDisplayInMap, tour) {
+                    if (tour.priority === 1) {
+                        toursToDisplayInMap.push(tour);
+                    }
+                    return toursToDisplayInMap;
+                }, []);
+
+                features = bikeTourMapService.buildToursStepTracesFeatures(toursToDisplayInMap, {
+                    style: {
+                        width: 3,
+                        weight: 6,
+                        opacity: 0.8
+                    },
+                    overStyle: {
+                        color: '#34a0b4',
+                        opacity: 1
+                    },
+                    callbacks: {
+                        'click': function (step, tour) {
+                            $scope.openTour(tour);
+                        }
+                    },
+                    label: function (step, tour) {
+                        return tour.title;
+                    }
+                });
+
+
+
+            }
+            return features;
+
+        }
+    }
+})();
