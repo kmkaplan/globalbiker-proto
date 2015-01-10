61f6d41082d0473c9c5e760831129c9fdebcc5e3
Nicolas Toublanc <n.toublanc@gmail.com>
2015-01-10 18:11:14 +0100

    Fix #60  Page step: bouton pour revenir au tour

--- Gruntfile.js
+++ Gruntfile.js
@@ -225,6 +225,8 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/public/{,*/}*.js',
             '<%= yeoman.dist %>/public/{,*/}*.css',
             '<%= yeoman.dist %>/public/assets/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}',
+            '!<%= yeoman.dist %>/public/assets/images/logo-globalbiker.png',
+            '!<%= yeoman.dist %>/public/assets/images/static/**',
             '<%= yeoman.dist %>/public/assets/fonts/*'
           ]
         }
@@ -688,4 +690,5 @@ module.exports = function (grunt) {
     'test',
     'build'
   ]);
+
 };
