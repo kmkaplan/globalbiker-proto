6b4b8387d10247eaeaa1bc8885840158c839fc9e
Nicolas Toublanc <n.toublanc@gmail.com>
2015-01-12 10:28:25 +0100

    Add piwik

--- client/app/app.js
+++ client/app/app.js
@@ -10,9 +10,34 @@ angular.module('globalbikerWebApp', [
   'leaflet-directive',
   'angularFileUpload',
   'pascalprecht.translate',
-  'ui.tinymce', 'ui.select'
+  'ui.tinymce',
+    'ui.select',
+    'angulartics', 'angulartics.piwik'
 ])
-    .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $translateProvider, uiSelectConfig) {
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $translateProvider, uiSelectConfig, $analyticsProvider) {
+
+
+        // Piwik seems to suppors buffered invocations so we don't need
+        // to wrap these inside angulartics.waitForVendorApi
+        $analyticsProvider.settings.trackRelativePath = true;
+        $analyticsProvider.registerPageTrack(function (path) {
+            if (window._paq) {
+                _paq.push(['setCustomUrl', path]);
+                _paq.push(['trackPageView']);
+            }
+        });
+        $analyticsProvider.registerEventTrack(function (action, properties) {
+            // PAQ requires that eventValue be an integer, see:
+            // http://piwik.org/docs/event-tracking/
+            if (properties.value) {
+                var parsed = parseInt(properties.value, 10);
+                properties.value = isNaN(parsed) ? 0 : parsed;
+            }
+            if (window._paq) {
+                _paq.push(['trackEvent', properties.category, action, properties.label, properties.value]);
+            }
+        });
+
 
         tinyMCE.baseURL = '/bower_components/tinymce';
 
